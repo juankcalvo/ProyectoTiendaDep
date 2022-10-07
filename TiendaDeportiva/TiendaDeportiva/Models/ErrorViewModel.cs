8ead6d16c9407eda5edd14c74ca5910e5f9e14b6
DayanaRomero08 <105830360+DayanaRomero08@users.noreply.github.com>
2022-10-07 17:24:00 -0600

    Se agrego hola mundo

---
+++ TiendaDeportiva/TiendaDeportiva/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TiendaDeportiva.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
